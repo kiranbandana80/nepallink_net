95f60bc6dc6d96d2c0bfb2085da7bede8d82049f
sarose <sarose@nepallink.net>
2017-08-25 16:46:29 +0545

    Added nodesj package

---
+++ dc-os-services.php
@@ -0,0 +1,103 @@
+<HTML>
+<HEAD>
+<TITLE>Nepal IT Jobs | Jobs in Nepal | Jobs at Nepallink | Programming Jobs Nepal | Nepal Web Programming Designer jobs | System Administrator Jobs in Nepal |  Linux System Administrator Job | Windows Server Administrator | MS Exchange Administrator | Application Server Administrator jobs | Customer Support jobs | sales marketing jobs in nepal</TITLE>
+<META http-equiv=Content-Language content=en-us>
+<META  name="description" content="NepalLink is a  leading  Web solutions company. Nepallink product ranges from high quality hosting services to cloud consulting, IT Administration, e-commerce solution, and dedicated web hosting as well as domain registration & web design.">
+<META name="keywords" content="Shared, Dedicated, Managed, Virtual, Business, Web, Hosting, Service, Reseller, Domain,  Internet, E-commerce, Windows, Unix, Linux, FreeBSD, Server, PHP, MySQL, ASP">
+<META  name="Abstract" content="Hosting, Web Hosting, Shared Web Hosting, Dedicated Web Hosting,aManaged Hosting, Reseller Web Hosting, Hosting Service, Business Web Hosting, Shared Hosting, Dedicated Hosting, Reseller Hosting, Dedicated Server, Internet Web Hosting, Domain Name Search, Domain Name Transfer, Domain Name Purchase, Domain Name Registration, Site Hosting, Data Center, E-commerce Hosting, Web Page Hosting, Windows Web Hosting, Web Hosting Sales, Web Hosting Services, Web Hosting Sales Chat, Web Hosting Solutions, Scalable Web Hosting, Stable Web Hosting, Windows 2000 Web Hosting, Linux Hosting, Linux Web Hosting, Unix Hosting, Unix Web Hosting, Reseller Program, Reseller Dedicated Hosting, Reseller Shared Hosting, Virtual Private Server, Cold Fusion Hosting, ColdFusion Hosting, ASP Web Hosting, ASP Hosting, PHP Hosting, Inexpensive Web Hosting, Web Hosting Discounts, SQL Hosting, MySQL Hosting, Web Hosting Company, Web Hosting Industry, SSL, POP E-mail, FrontPage Extensions">
+<META name="srce" content="http://www.nepallink.net">
+<META name="robots" content="index/follow">
+<META name="revisit" content="7 days">
+<META name="distribution" content="global">
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+<LINK REL="Stylesheet" href="style.css" type="text/css">
+<style type="text/css">
+<!--
+.style4 {
+	font-size: 16px;
+	font-weight: bold;
+}
+.style5 {
+	font-size: 14px;
+	font-weight: bold;
+}
+.style7 {font-size: 14px}
+-->
+</style>
+</HEAD>
+
+<BODY BGCOLOR=#E4E5EA LEFTMARGIN=0 TOPMARGIN=2 MARGINWIDTH=0 MARGINHEIGHT=0 BACKGROUND="images/bg.gif">
+<CENTER>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14" align="left" valign="top" background="images/left.gif">&nbsp;</td>
+    <td width="747" align="right" valign="middle" bgcolor="#E5E7E8" height="20">Nepalilnk expertise in offering K8s, Mesos, Containers and DC/OS deployment design and implementation.</td>
+    <td width="14" align="left" valign="top" background="images/right.gif">&nbsp;</td>
+  </tr>
+</table>
+
+   <?php
+include_once("header.php");
+?>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14" align="left" valign="top" background="images/left.gif"><img src="images/spacer.gif" height="15" width="10" alt=""></td>
+    <td width="747" align="left" valign="top" background="images/bkg.gif"><img src="images/spacer.gif" height="15" width="10" alt=""></td>
+    <td width="14" align="left" valign="top" background="images/right.gif"><img src="images/spacer.gif" height="15" width="10" alt=""></td>
+  </tr>
+</table>
+
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14"  align="left" valign="top" background="images/left.gif">&nbsp;</td>
+    <td width="747" align="left" valign="top" bgcolor="#FFFFFF">
+
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+    <td width="9%" align="left" valign="top"><br />
+     
+	</td>
+    <td width="90%" align="left" valign="top"><div align="center"><span class="style4">Career Opportunities</span><br />
+          <br />
+    </div>
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+    <td align="left" valign="top"><div align="justify">
+        <p class="style5">Mesos, Containers and DC/OS Services</p>
+        <p>Our intent was to solve very specific problems in our environment, that we felt containers could help us solve:</p>
+        <p><strong>Improved Efficiency. <br>
+          </strong>We need to improve the overall resource utilization in our data centers to aid in cost reduction as well as fully optimizing the use of our infrastructure.<br>
+          <br>
+          <strong>Data Center Abstraction. <br>
+          </strong>With a hybrid data center strategy, we want to abstract away the underlying data center provider and enable our platform services to be seamlessly deployed and able to run across multiple providers in the same way.<br>
+          <br>
+          <strong>Developer Efficiency. <br>
+          </strong>By standardizing our CI/CD process on top of a container-based infrastructure we felt we could more rapidly deploy updates and significantly simplify the developer experience for building services.</p>
+        <p>&nbsp;</p>
+        <p>http://cloudengineering.autodesk.com/blog/2016/04/autodesk-is-forging-ahead-with-dcos.html<br>
+        </p>
+        </div></td>
+    <td width="10%" align="left" valign="top">&nbsp;</td>
+  </tr>
+</table>
+	</td>
+  </tr>
+</table>
+
+
+    </td>
+    <td width="14" align="left" valign="top" background="images/right.gif">&nbsp;</td>
+  </tr>
+</table>
+
+<?php
+include_once("footer.php");
+?>
---
+++ services/Nodejs_MongoDB_Hosting.php
@@ -0,0 +1,424 @@
+<HTML>
+<HEAD>
+<TITLE>Nodejs   High Quality  Hosting  | MongoDB Hosting | Node Hosting  | Nodejs NGINX Mongo Database</TITLE>
+<META http-equiv=Content-Language content=en-us>
+<META  name="description" content=" Cheap High quality Nodejs and Mongo hosting plans optimized for the rapid and reliable deployment of Nodejs web applications. ">
+<META name="keywords" content="Nodejs, Node,  Nodejs Hosting, Node and MongoDB Hosting, Passenger, Nginx, nodejs cluster, FastCGI, Javascirpt">
+<META name="author" content="Nepallink Network">
+<META name="srce" content="http://www.nepallink.net">
+<META name="robots" content="index/follow">
+<META name="revisit" content="7 days">
+<META name="distribution" content="global">
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+<LINK REL="Stylesheet" href="../style.css" type="text/css">
+</HEAD>
+<style type="text/css">
+.mytd1{
+	color:#000000;
+	background:#E6E6E6;
+	text-align:center;
+	
+	}
+.mytd{
+	color:#000000;
+	background:#EDF4F8;
+	text-align:center;
+	
+}
+.mytr{
+color:#FFFFFF;
+background:#003366;
+text-align:center;
+font:bold;
+
+}
+</style>
+
+<BODY BGCOLOR=#E4E5EA LEFTMARGIN=0 TOPMARGIN=2 MARGINWIDTH=0 MARGINHEIGHT=0 BACKGROUND="../images/bg.gif">
+<CENTER>
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14" align="left" valign="top" background="../images/left.gif">&nbsp;</td>
+    <td width="747" align="right" valign="middle" bgcolor="#E5E7E8" height="20"> Realtime and Scalable Application Platform - Nodejs and MongoDB  </td>
+    <td width="14" align="left" valign="top" background="../images/right.gif">&nbsp;</td>
+  </tr>
+</table>
+
+
+		<?php include("../header.php"); ?>
+ 
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14" align="left" valign="top" background="../images/left.gif"><img src="../images/service/spacer.gif" height="15" width="10" alt=""></td>
+    <td width="747" align="left" valign="top" background="../images/bkg.gif"><img src="../images/service/spacer.gif" height="15" width="10" alt=""></td>
+    <td width="14" align="left" valign="top" background="../images/right.gif"><img src="../images/service/spacer.gif" height="15" width="10" alt=""></td>
+  </tr>
+</table>
+
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="775">
+  <tr>
+    <td width="14" align="left" valign="top" background="../images/left.gif">&nbsp;</td>
+    <td width="747" align="left" valign="middle" bgcolor="#FFFFFF">
+
+
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+    <td width="35%" align="left" valign="top">&nbsp;&nbsp;<font class="title"> Node.js &amp; MongoDB Hosting</font><br>
+	<br>
+	<ul><p><img src="../images/nodejs.gif" width="100" height="30" alt=""/>
+	  <p>Node.js&reg; is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world.
+</p>
+ <p>Nepallink offers one the easiest one click Node.js deployment platform at affordable price range. </p>
+ <p><img src="../images/nginx.gif" width="75" height="60" alt=""/></p>
+ <p>Nginx is best known for its speed and delivering superior performance even under massive traffics loads. NGINX has been designed and tuned for high performance under difficult conditions. We run nginx  as reverse proxy service for nodejs. Since both node.js and nginx is event-based, which means that it does not need to spawn a new thread or process for each request. This means that the number of concurrent requests has almost no effect on NGINX and node.js performance.</p>
+ <p><img src="../images/mongodb.gif" width="100" height="30" alt=""/></p>
+ <p>MongoDB is schema less, document based database in which one collection holds different documents.We offer   scalable, high-performance mongodb cluster without additional cost. Unlike MySQL, MogoDb offers superior features for demanding web applicaiton -</p>
+ <p>* Auto-sharding<br>
+   * Native replication<br>
+   * In-memory speed<br>
+   * Embedded data model<br>
+   * Comprehensive secondary indexes
+ </p>
+ <p>&nbsp; </p>
+ <p><img src="../images/git.gif" width="100" height="41" alt=""/></p>
+ <p>To make deploying your node.js even easier, We have introduced integration with Git &ndash; the most popular source code management system.</p>
+ <p>Now Easily deploy your website content by either pushing it to a local Git repository or by pulling from a remote one with a single click. No command line.<br>
+   <br>
+   Use GitHub, BitBucket, Travis, or any other software engineering service of your choice.<br>
+   Create multiple branches in a single repository &ndash; useful for separating staging and production code.<br>
+   <br>
+   Deploy manually for complete control or enable automatic deployment to save time.              </p>
+                    <p><strong>Try our other Hosting Technologies<br>
+                      <br>
+                    <a href="javahosting.php"><img src="../images/java-logo.gif" width="69" height="88" alt=""/></a> </strong>                    <br></p>
+                    <p><a href="dotnet.php"><img src="../images/fdotneticon.jpg" width="88" height="33" alt=""/></a><br>
+                      <br>
+                    </p>
+              <p><a href="special_offer.php"><img src="../images/fphpicon.jpg" width="85" height="43" alt=""/></a><br>
+              </p>
+                    <p><strong><br>
+                    </strong></p>
+                    <p><strong>Try Our 100% Managed Servers</strong></p>
+                    <p><img src="../images/windows_logo.png" width="82" height="67" alt=""/></p>
+              <p><a href='https://nepallink.net/windows-cloud-server.php'>Windows Managed Server</a></p>
+                    <p><img src="../images/linux_logo.png" width="96" height="95" alt=""/></p>
+                    <p><a href='https://www.nepallink.net/vps.php'>Linux Manged Server</a></p>
+	</ul>
+	<p>&nbsp;</p>  
+
+	</td>
+    <td width="5%" align="left" valign="top" class="rightborder">&nbsp;</td>
+    <td width="60%" align="left" valign="top"><img src="../images/service/aboutus.jpg"><br>
+      <table width="100%" height="0" border="0" cellpadding="0" cellspacing="0">
+  <!--DWLayoutTable-->
+  <tr>
+    <td width="23" >&nbsp;</td>
+    <td width="393" align="left" valign="top" >      <div align="justify">
+      <h2> Professional Node.js Hosting</h2>
+      <p>Nodejs is the fastest growing development platform. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.</p>
+      <p>Our NodeJS hosting comes with MongoDB, MySQL and GIT Integration.</p>
+      <h3>Node.js Bronze</h3>
+      <table width="100%" border="0" cellspacing="1" cellpadding="4" id="table1">
+        <tr>
+          <td width="70%" bgcolor="#003366"><p align="left"> <font color="#FFFFFF"> <b> Features</b></td>
+          <td class="eee" valign="center" align="middle" width="30%" bgcolor="#003366" height="1">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Disk Storage</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">10GB RAID-1</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Real-time redundancy</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">200Mbps</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Monthly Datatransfer</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">5TB</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Mulitple Nodejs version</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes </td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8"><span class="ff">Nodejs Package Manager (NPM</span></td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Easy GIT integration</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+
+
+        <tr>
+          <td bgcolor="#EDF4F8">Easy Continuous Deployment (CD)</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">MongoDB Database</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6">4</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">MySQL Database</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> 4</td>
+        </tr>
+        
+
+        <tr>
+          <td bgcolor="#EDF4F8">Subdomain</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">2</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Main Domain</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">1</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">SSH Access</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">NGINX Powered</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Cloudflare Ready</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">SELinux enabled OS</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Free SSL Certificates</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+
+        <tr>
+          <td bgcolor="#EDF4F8">2-Factor Authentication</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Highly Avaialble DNS</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">&nbsp; 
+            Yearly Cost </td>
+          <td align="middle" width="30%" height="7" bgcolor="#E6E6E6"><b> USD 30</b></td>
+        </tr>
+
+        <tr>
+          <td width="70%">&nbsp; 
+            Order Online <span class="ff"> &raquo;</span></td>
+          <td valign="center" align="middle" width="30%" height="17" bgcolor="#FFFFFF"><a href="../order.php?plan=NODE_BROZNE_HOSTING"><img src='http://www.nepallink.net/images/order_blue.gif' border='0'></a></td>
+        </tr>
+      </table>
+      <h3>                 Node.js Gold</h3>
+      <table width="100%" border="0" cellspacing="1" cellpadding="4" id="table2">
+        <tr>
+          <td width="70%" bgcolor="#003366"><p align="left"> <font color="#FFFFFF"> <b> Features</b></td>
+          <td class="eee" valign="center" align="middle" width="30%" bgcolor="#003366" height="1">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Disk Storage</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">40GB RAID-1</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Real-time redundancy</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">500Mbps</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Monthly Datatransfer</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">10TB</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Mulitple Nodejs version</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes </td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8"><span class="ff">Nodejs Package Manager (NPM</span></td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Easy GIT integration</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Easy Continuous Deployment (CD)</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">MongoDB Database</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6">8</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">MySQL Database</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> 8</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Subdomain</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">4</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Main Domain</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">4</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">SSH Access</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">NGINX Powered</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">Cloudflare Ready</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">SELinux enabled OS</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Free SSL Certificates</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td bgcolor="#EDF4F8">2-Factor Authentication</td>
+          <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">Highly Avaialble DNS</td>
+          <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+        </tr>
+        <tr>
+          <td width="70%" bgcolor="#EDF4F8">&nbsp; 
+            Yearly Cost </td>
+          <td align="middle" width="30%" height="7" bgcolor="#E6E6E6"><b> USD 150</b></td>
+        </tr>
+        <tr>
+          <td width="70%">&nbsp; 
+            Order Online <span class="ff"> &raquo;</span></td>
+          <td valign="center" align="middle" width="30%" height="17" bgcolor="#FFFFFF"><a href="../order.php?plan=NODE_GOLD_HOSTING"><img src='http://www.nepallink.net/images/order_blue.gif' border='0'></a></td>
+        </tr>
+      </table>
+      <p>&nbsp;</p>
+<h3>Node.js Platinum </h3>
+<table width="100%" border="0" cellspacing="1" cellpadding="4" id="table3">
+  <tr>
+    <td width="70%" bgcolor="#003366"><p align="left"> <font color="#FFFFFF"> <b> Features</b></td>
+    <td class="eee" valign="center" align="middle" width="30%" bgcolor="#003366" height="1">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Disk Storage</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">200GB  RAID-1</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Real-time redundancy</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">1Gbps</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Monthly Datatransfer</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Unlimited</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">Mulitple Nodejs version</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes </td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8"><span class="ff">Nodejs Package Manager (NPM</span></td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">Easy GIT integration</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Easy Continuous Deployment (CD)</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">MongoDB Database</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6">Unlimited</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">MySQL Database</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Unlimited</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Subdomain</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">20</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Main Domain</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">10</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">SSH Access</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">NGINX Powered</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">Cloudflare Ready</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">SELinux enabled OS</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">Free SSL Certificates</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+  </tr>
+  <tr>
+    <td bgcolor="#EDF4F8">2-Factor Authentication</td>
+    <td align="middle" height="13" bgcolor="#E6E6E6">Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">Highly Avaialble DNS</td>
+    <td align="middle" width="30%" height="13" bgcolor="#E6E6E6"> Yes</td>
+  </tr>
+  <tr>
+    <td width="70%" bgcolor="#EDF4F8">&nbsp; 
+      Yearly Cost </td>
+    <td align="middle" width="30%" height="7" bgcolor="#E6E6E6"><b> USD 300</b></td>
+  </tr>
+  <tr>
+    <td width="70%">&nbsp; 
+      Order Online <span class="ff"> &raquo;</span></td>
+    <td valign="center" align="middle" width="30%" height="17" bgcolor="#FFFFFF"><a href="../order.php?plan=NODE_PLATINUM_HOSTING"><img src='http://www.nepallink.net/images/order_blue.gif' border='0'></a></td>
+  </tr>
+</table>
+    </div></td>
+    <td width="33" >&nbsp;</td>
+  </tr>
+</table>
+
+
+<br>
+
+
+<ul>
+<div align="left"></div>
+	</ul>
+
+	</td>
+  </tr>
+</table>
+
+
+<p>&nbsp;</p>
+
+</td>
+    <td width="14" align="left" valign="top" background="../images/right.gif">&nbsp;</td>
+  </tr>
+</table>
+<?php  include_once("../footer.php"); ?>
