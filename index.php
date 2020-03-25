09d043d1a70eccd58ccd23819ebf40fea99917f9
ppradhananga <ppradhananga@berrybytes.com>
2020-03-25 17:23:20 +0545

    modal added on homepage

--- index.php
+++ index.php
@@ -1059,6 +1059,16 @@ language="javascript" src="splashpage.js"></script>
   <tr>
    
   </tr>
-</table>              
+</table>       
+
+<div class="modal" id="covid" aria-hidden="true">
+  <div class="modal-dialog">
+    <a href="#covid" class="btn-close" aria-hidden="true">×</a>
+   <div class="modal-body">
+    <img src="images/covid.png" width="460px" alt="stay home stay safe">
+   </div>
+ </div>
+</div>   
+    
 </body>
 </html>
